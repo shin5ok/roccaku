09180fc6e818cc0d9576b64bc2823cc487f8a7f0
shin5ok <shin5ok@55mp.com>
2014-04-11 17:39:34 +0900

    add a module for env

---
+++ lib/Roccaku/Run/Env.pm
@@ -0,0 +1,13 @@
+package Roccaku::Run::Env;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+use base qw( Roccaku::Run::Base );
+
+sub run {
+  my ($self) = @_;
+}
+
+1; # End of Roccaku::Env;
