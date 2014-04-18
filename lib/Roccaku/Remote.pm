76bd6beef7ae8942107b0f13c43c487cc76c1fe0
shin5ok <shin5ok@55mp.com>
2014-04-18 22:18:44 +0900

    implement remote run

--- lib/Roccaku/Remote.pm
+++ lib/Roccaku/Remote.pm
@@ -4,13 +4,14 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
-use File::Temp;
+use POSIX qw(strftime);
 use FindBin;
+use File::Basename;
 use lib qq($FindBin::Bin/../lib);
 
 use Roccaku::Utils;
 
-my $sudo = qq{};
+our $sudo = qq{};
 
 our $temporary_working_base = q{/var/tmp};
 
@@ -19,20 +20,26 @@ sub run {
   my $temporary_working_dir = _gen_working_dir();
   my $scp = "$sudo scp -r $path/ $host:$temporary_working_dir/$path";
   my $run = sprintf "$sudo ssh %s %s/bin/roccaku %s",
-                 $host,
-                 $path,
-                 ( join " ", @$command_argv );
-  logging $scp;
-  system $scp;
-  logging $run;
-  system $run;
-  system "rm -Rf $temporary_working_dir";
+                     $host,
+                     $path,
+                     ( join " ", @$command_argv );
+  my $rmd = "rm -Rf $temporary_working_dir";
+  push @cmds, ( qq{$scp}, qq{$run}, qq{$rmd} );
+
+  for my $cmd ( @cmds ) {
+    my $r = (system $cmd) == 0
+          ? "ok"
+          : "ERROR";
+
+    logging "$r: $cmd";
+  }
 }
 
 sub _gen_working_dir {
-  my $name = `uuidgen`;
-  chomp $name;
-  sprintf "%s/%s", $temporary_working_base, $name;
+  my $uuid = `uuidgen 2> /dev/null`;
+  chomp $uuid;
+  $uuid ||= strftime "%Y%m%d%H%M%S", localtime;
+  sprintf "%s/.%s_%s", $temporary_working_base, basename $0, $uuid;
 }
 
 1; # End of Roccaku::Say;
