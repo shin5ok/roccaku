c3667b82ec91424a317812d301e143d356b99339
shin5ok <shin5ok@55mp.com>
2014-04-18 15:11:26 +0900

    modified to use sort number at generating code for remote

--- lib/Roccaku/Remote.pm
+++ lib/Roccaku/Remote.pm
@@ -10,33 +10,49 @@ use File::Basename;
 use FindBin;
 use lib qq($FindBin::Bin/../lib);
 
-our $code = qq{};
+our %gen_code;
 
 sub run {
   my ($self) = @_;
 
-  $code ||= get_datetime_comment();
+  my $code ||= get_datetime_comment();
 
   my $lib_path = dirname ( File::Spec->rel2abs( __FILE__ ) );
   find( \&generating_code, "$lib_path/../" );
 
-  $code .= "1;\n";
+  use Data::Dumper; warn Data::Dumper::Dumper ( \%gen_code );
+
+  # $code .= "1;\n";
 
 }
 
 sub generating_code {
   my $f = $File::Find::name;
-  (-f $f and $f =~ /\.pm$/) or return;
+
+  (defined $f and -f $f and $f =~ /\.pm$/)
+    or return;
+
   open my $fh, "<", $f
     or return;
+  # default 100
+  my $gen_sort_num = 100;
+  my $code = qq{};
   while (my $line = <$fh>) {
+    if ($line =~ m{^\s* my \s+ \$GEN_SORT \s* \= \s* (\d+) }x) {
+      $gen_sort_num = eval { $1 };
+    }
     $line =~ m{^1\s*\;\s*\#} and next;
     $line =~ m{\s?(?:use|require)\s+Roccaku} and next;
     $line =~ s{Roccaku}{Roccaku_x}g;
     $code .= $line;
   }
-  $code .= "\n";
   close $fh;
+
+  $code .= "\n";
+
+  no strict 'refs';
+  $gen_code{$gen_sort_num} .= $code;
+
   return;
 }
 
