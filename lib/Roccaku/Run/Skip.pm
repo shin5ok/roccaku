0fbfe1bdeb0a7540c11f3b507f2e5e6ea5846b77
shin5ok <shin5ok@55mp.com>
2014-04-25 13:22:09 +0900

    add a Skip based Must

---
+++ lib/Roccaku/Run/Skip.pm
@@ -0,0 +1,14 @@
+package Roccaku::Run::Skip;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+use FindBin;
+use lib  qq($FindBin::Bin/../lib);
+use Roccaku::Run::Must;
+use base qw( Roccaku::Run::Must );
+
+our $__GEN_SORT = 52;
+
+1; # End of Roccaku::Skip;
